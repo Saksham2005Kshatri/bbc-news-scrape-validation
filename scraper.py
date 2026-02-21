f8e650c7f332db776712119bca7bd8fd7feab27a
Saksham2005Kshatri <sakshamkshatri5@gmail.com>
2026-02-21 21:52:55 +0545

    fixed bugs added functionality to skip links that exceed time limit

--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
-import asyncio 
-from playwright.async_api import async_playwright
+import asyncio
+from playwright.async_api import async_playwright, TimeoutError as PlaywrightTimeoutError
 import csv
 import json
 import sys
@@ -33,71 +33,83 @@ async def extract_technology_news(page):
             writer.writeheader()
 
 
-    while current_page <= 7:
-        # click current page button
-        buttons_section = page.get_by_test_id("pagination")
-        button = buttons_section.get_by_role(f"//button[title='Go to page {current_page}']")
+    while True:
+        print(f"Scraping page: {current_page}")
+
         articles = await more_articles_section.locator('//div[@data-testid="anchor-inner-wrapper"]').all()
 
         for article in articles:
             a_element = article.get_by_test_id("internal-link").first
             if a_element:
                 href_a = await a_element.get_attribute("href")
-                # https://www.bbc.com/news/articles/cn9z0d9pd2xo
                 complete_link = f"https://www.bbc.com{href_a}"
                 all_present_links.append(complete_link)
 
+        # check if a next page button exists
+        buttons_section = page.get_by_test_id("pagination")
+        next_button = buttons_section.get_by_text(f"{current_page + 1}")
+        if await next_button.count() == 0:
+            break
 
-        current_page += 1 
+        await next_button.click()
+        await asyncio.sleep(2)
+        current_page += 1
+
+    print(f"Total links collected: {len(all_present_links)}")
   
     
     for link in all_present_links:
-        await page.goto(link, timeout=30000)
-        headline_block_locator =  page.locator('//div[@data-component="headline-block"]').first
-        title = "no title found"
-        if await headline_block_locator.locator('h1').count() > 0:
-            title = await headline_block_locator.locator("h1").first.inner_text()
+        try:
+            await page.goto(link, timeout=30000)
+            headline_block_locator =  page.locator('//div[@data-component="headline-block"]').first
+            title = "no title found"
+            if await headline_block_locator.locator('h1').count() > 0:
+                title = await headline_block_locator.locator("h1").first.inner_text()
 
-        author = None
-        byline_contributor_block_locator = page.get_by_test_id("byline-contributors-contributor-0") 
-        author = await byline_contributor_block_locator.locator("span").first.inner_text()
+            author = None
+            byline_contributor_block_locator = page.get_by_test_id("byline-contributors-contributor-0")
+            author = await byline_contributor_block_locator.locator("span").first.inner_text()
 
-        # logic to extract date time
-        byline_block_locator = page.get_by_test_id("byline").first
-        time_locator = byline_block_locator.locator("time").first
-        if time_locator:
-            full_date_time = await time_locator.get_attribute('datetime')
-        
-        date_formatted = full_date_time.split('T')[0]
+            # logic to extract date time
+            byline_block_locator = page.get_by_test_id("byline").first
+            time_locator = byline_block_locator.locator("time").first
+            if time_locator:
+                full_date_time = await time_locator.get_attribute('datetime')
 
-        # extracting article id
-        split_link = link.split('/')
-        article_id = split_link[-1]
+            date_formatted = full_date_time.split('T')[0]
 
-        # extracting image
-        image_url = "no image found"
-        hero_image_locator = page.get_by_test_id("hero-image").first
-        if await hero_image_locator.count() > 0:
-            image_element = hero_image_locator.locator("img").first
-            if await image_element.count() > 0:
-                srcset = await image_element.get_attribute("srcset")
-                if srcset:
-                    image_url = srcset.split(' ')[0]
+            # extracting article id
+            split_link = link.split('/')
+            article_id = split_link[-1]
 
-        # dictionary to append
-        article = {
-            "title": title, 
-            "author": author, 
-            "date": date_formatted, 
-            "image_url": image_url,
-            "article_id": article_id, 
-            "link": link
-        }
+            # extracting image
+            image_url = "no image found"
+            hero_image_locator = page.get_by_test_id("hero-image").first
+            if await hero_image_locator.count() > 0:
+                image_element = hero_image_locator.locator("img").first
+                if await image_element.count() > 0:
+                    srcset = await image_element.get_attribute("srcset")
+                    if srcset:
+                        image_url = srcset.split(' ')[0]
 
-        # append dictionary to file
-        append_data(article, filename, COLUMNS)
-        
-        print(f"TITLE: {title}, AUTHOR: {author}, DATE: {date_formatted}, Article ID: {article_id}")
+            # dictionary to append
+            article = {
+                "title": title,
+                "author": author,
+                "date": date_formatted,
+                "image_url": image_url,
+                "article_id": article_id,
+                "link": link
+            }
+
+            # append dictionary to file
+            append_data(article, filename, COLUMNS)
+
+            print(f"TITLE: {title}, AUTHOR: {author}, DATE: {date_formatted}, Article ID: {article_id}")
+
+        except PlaywrightTimeoutError:
+            print(f"SKIPPED (timeout): {link}")
+            continue
 
 
 def check_file_format(filename):
